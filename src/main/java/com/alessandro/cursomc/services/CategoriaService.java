898c0644cf35060be6c76f21f161c20d36456683
alesbernardes <alesbernardes@gmail.com>
2019-02-03 15:44:39 -0200

    Validacao sintatica com bean validadtion

--- src/main/java/com/alessandro/cursomc/services/CategoriaService.java
+++ src/main/java/com/alessandro/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.alessandro.cursomc.domain.Categoria;
+import com.alessandro.cursomc.dto.CategoriaDTO;
 import com.alessandro.cursomc.repositories.CategoriaRepository;
 import com.alessandro.cursomc.services.exceptions.DataIntegrityException;
 import com.alessandro.cursomc.services.exceptions.ObjectNotFoundException;
@@ -52,5 +53,8 @@ public class CategoriaService {
 
 		return repo.findAll(pageRequest);
 
+	}
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
