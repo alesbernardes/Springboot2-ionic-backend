1df131a59bd8654da56424a42bc205542484f052
alesbernardes <alesbernardes@gmail.com>
2019-02-05 12:55:35 -0200

    validação customizada email

--- src/main/java/com/alessandro/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/alessandro/cursomc/dto/ClienteDTO.java
@@ -2,13 +2,16 @@ package com.alessandro.cursomc.dto;
 
 import java.io.Serializable;
 
+
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 
 import org.hibernate.validator.constraints.Length;
 
 import com.alessandro.cursomc.domain.Cliente;
+import com.alessandro.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
